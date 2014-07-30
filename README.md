77aab1760ee5307c6623e385429e213ba51227d7
Jamis Johnson <jamismanwaring@gmail.com>
2014-07-30 00:38:51 -0400

    README changes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-A notification center for ziggy builds.
+Flask + Heroku starter kit for the [Yo api](https://medium.com/@YoAppStatus/yo-developers-api-e7f2f0ec5c3c).
 
-First goal: a Yo! everytime ziggy finishes crawling!
+### Setup
+
+1. Setup an app on heroku
+
+2. Signup for the [Yo api](http://yoapi.justyo.co/) and use the Heroku url as the callback, ie http://my-new.herokuapp.com/yo.
+
+3. After pushing to heroku, run `heroku config:set YO_KEY=<KEY>`.
+
+4. PrYofit
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os, requests
 from flask import Flask
-from tokens import YO_TOKEN
 
 app = Flask(__name__)
 
+try: import env
+except ImportError:
+    # no env module but heroku may have set env var
+    try: os.environ['YO_KEY']
+    except KeyError:
+        print 'Dawg, you need to set your YO_KEY!'
+
 @app.route('/yo')
 def yo():
-    token = { "api_token": YO_TOKEN }
-    yo_all = "http://api.justyo.co/yoall/"
-    r = requests.post(yo_all, data=token)
+    token = { "api_token": os.environ['YO_KEY'] }
+    yoall = "http://api.justyo.co/yoall/"
+    requests.post(yoall, data=token)
     return 'Yo!'
